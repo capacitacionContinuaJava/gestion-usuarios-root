0cf91cd9619eee27615e3f5ff55309abd6042a4f
RICHARD\RICHARD <richardeah97gmail.com>
2022-04-02 09:42:37 -0500

    subo archivos pendientes.

---
+++ src/main/java/ec/com/gestion/gestionusuariosroot/GestionUsuariosRootApplication.java
@@ -0,0 +1,13 @@
+package ec.com.gestion.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUsuariosRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionUsuariosRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/com/gestion/gestionusuariosroot/GestionUsuariosRootApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.gestion.gestionusuariosroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUsuariosRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
